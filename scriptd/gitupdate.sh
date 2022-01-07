104572de7eff047c3344c77a7369ac37eb775302
Dima H <dima@hasikimi.com>
2022-01-07 14:36:20 +0800

    updated git via script

--- scriptd/gitupdate.sh
+++ scriptd/gitupdate.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-#pull
+function pause(){
+     read -p "$*"
+   }
+  
+ # ...
+ # call it
+ pause 'Press [Enter] key to continue...'
+ # rest of the script
+ # ...
+
+#pull script
 
 # store the current dir
 CUR_DIR=$(pwd)
